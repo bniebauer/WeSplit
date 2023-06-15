b98adade29c7def1e7d5b48cb32d6329fc1296fb
Brenton Niebauer <b.niebauer@me.com>
2023-06-14 23:03:16 -0400

    Finished product

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountHasFocus: Bool
+    
+    let tipPercentages = [ 15, 20, 25, 30, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipAmount = amount / 100 * tipSelection
+        
+        let totalAmount = amount + tipAmount
+        let perPerson = totalAmount / peopleCount
+        return perPerson
+    }
+    
     var body: some View {
-        Text("WeSplit")
-        .padding()
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Check amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountHasFocus)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("What percentage for tip?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Total per person")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountHasFocus = false
+                    }
+                }
+            }
+        }
     }
 }
 
